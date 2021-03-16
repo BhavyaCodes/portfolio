6cc3b6357812ca201508e2eac51590b85ed3469c
Bhavya <xbhavyatomar@gmail.com>
2021-03-16 22:04:45 +0530

    remove comments

--- hooks/useLocalStorage.tsx
+++ hooks/useLocalStorage.tsx
@@ -1,39 +1,3 @@
-// import { useState, useEffect, Dispatch, SetStateAction } from "react";
-
-// function useLocalStorage(
-//   key: string,
-//   initialVal: boolean
-// ): [boolean, Dispatch<SetStateAction<boolean>>] {
-//   const [state, setState] = useState<boolean>(() => {
-//     try {
-//       if (window.localStorage.getItem(key) === "true") {
-//         return true;
-//       } else {
-//         return false;
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-
-//     // window.localStorage.setItem(key, initialVal.toString());
-
-//     return initialVal;
-//   });
-
-//   useEffect(() => {
-//     if (state) {
-//       try {
-//         window.localStorage.setItem(key, state.toString());
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     }
-//   }, [state, key]);
-
-//   return [state, setState];
-// }
-
-// export default useLocalStorage;
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
 function useLocalStorage(
