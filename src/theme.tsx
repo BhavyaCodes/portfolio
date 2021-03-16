455563884bc884c03f2b83df78cf4cc4794e9415
Bhavya <xbhavyatomar@gmail.com>
2021-03-16 16:27:16 +0530

    remove cutome mui theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,21 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
+// import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#fff",
-    },
+    // primary: {
+    //   main: "#556cd6",
+    // },
+    // secondary: {
+    //   main: "#19857b",
+    // },
+    // error: {
+    //   main: red.A400,
+    // },
+    // background: {
+    //   default: "#fff",
+    // },
   },
 });
 
