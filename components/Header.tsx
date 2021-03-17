014f4d1a9088f4c2c037602b16dd32720ff3ebd0
Bhavya <xbhavyatomar@gmail.com>
2021-03-17 19:12:03 +0530

    add grid to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,14 @@
 import { useContext } from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
-import { Toolbar } from "@material-ui/core";
+import { Toolbar, Grid, Box, Container } from "@material-ui/core";
+import programmer from "../assets/programmer.svg";
 
-import {
-  DarkModeContext,
-  DispatchDarkModeContext,
-} from "../context/themeContext";
+import { DarkModeContext } from "../context/themeContext";
 import lightHeader from "../assets/light-header.svg";
 import darkHeader from "../assets/dark-header.svg";
 
 function Header() {
   const darkMode = useContext(DarkModeContext);
-  const setDarkMode = useContext(DispatchDarkModeContext);
 
   const useStyles = makeStyles((_theme: Theme) =>
     createStyles({
@@ -19,24 +16,37 @@ function Header() {
         height: "100vh",
         backgroundImage: `url(${darkMode ? darkHeader : lightHeader})`,
         backgroundSize: "cover",
+        display: "flex",
+        flexDirection: "column",
+      },
+      container: {
+        flexGrow: 1,
+        display: "flex",
+      },
+      headerImg: {
+        maxWidth: "90%",
+      },
+      gridItem: {
+        display: "flex",
+        alignContent: "center",
       },
     })
   );
-
-  const handleThemeChange = () => {
-    if (setDarkMode) {
-      setDarkMode((prevState) => {
-        return !prevState;
-      });
-    }
-  };
 
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Toolbar id="back-to-top-anchor" />
-      header
-      <button onClick={handleThemeChange}>Change Theme</button>
+      <Container className={classes.container}>
+        <Grid container>
+          <Grid item md={6} xs={12} className={classes.gridItem}>
+            <div></div>
+          </Grid>
+          <Grid item md={6} xs={12} className={classes.gridItem}>
+            <img className={classes.headerImg} src={programmer} />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
