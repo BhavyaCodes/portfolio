6c70693b3393c880fb5ad200ebf19a04e8f28863
Bhavya <xbhavyatomar@gmail.com>
2021-03-18 23:54:03 +0530

    display icons in grid

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,18 +1,91 @@
-import React from "react";
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Container } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+import Icon from "./Icon";
+
+import git from "assets/icons/git.svg";
+import github from "assets/icons/github.svg";
+import javascript from "assets/icons/javascript.svg";
+import materialui from "assets/icons/materialui.svg";
+import mongodb from "assets/icons/mongodb.svg";
+import nextjs from "assets/icons/nextjs.svg";
+import nodejs from "assets/icons/nodejs.svg";
+import postgresql from "assets/icons/postgresql.svg";
+import react from "assets/icons/react.svg";
+import redux from "assets/icons/redux.svg";
+import sass from "assets/icons/sass.svg";
+import typescript from "assets/icons/typescript.svg";
+import expressjs from "assets/icons/expressjs.png";
+import bootstrap from "assets/icons/bootstrap.svg";
 
 function Skills() {
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      gridItem: {
+        marginBottom: theme.spacing(3),
+      },
+    })
+  );
+  const classes = useStyles();
   return (
-    <Box mt={5}>
+    <Container>
       <Grid container>
         <Grid xs={12} md={6} item>
           some svg here
         </Grid>
         <Grid xs={12} md={6} item>
-          <Typography variant="h2">My Tech Stack</Typography>
+          <Typography align="center" variant="h2" gutterBottom>
+            My Tech Stack
+          </Typography>
+          <div>
+            <Grid container>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="git" logo={git} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon invert={true} title="github" logo={github} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="javascript" logo={javascript} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="materialui" logo={materialui} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="mongodb" logo={mongodb} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon invert={true} title="nextjs" logo={nextjs} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="nodejs" logo={nodejs} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="postgresql" logo={postgresql} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon spin={true} title="react" logo={react} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="redux" logo={redux} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="sass" logo={sass} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="typescript" logo={typescript} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="expressjs" logo={expressjs} />
+              </Grid>
+              <Grid item className={classes.gridItem} xs={2}>
+                <Icon title="bootstrap" logo={bootstrap} />
+              </Grid>
+            </Grid>
+          </div>
         </Grid>
       </Grid>
-    </Box>
+    </Container>
   );
 }
 
