c942228d2a5012f2b4a682f83565826200e2ba41
Bhavya <xbhavyatomar@gmail.com>
2021-03-20 17:02:51 +0530

    setup react-linkify

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -1,23 +1,44 @@
-import { Grid, Box } from "@material-ui/core";
+import { Grid, Box, Typography } from "@material-ui/core";
+import Linkify from "react-linkify";
 
 type AppProps = {
   index: number;
+  title: string;
+  description: string;
 };
 
-function Project({ index }: AppProps) {
+function Project({ index, title, description }: AppProps) {
   return (
-    <Grid container>
-      <Box clone order={{ xs: 2, md: index % 2 === 0 ? 2 : 1 }}>
-        <Grid item md={6} xs={12}>
-          text here
-        </Grid>
-      </Box>
-      <Box clone order={{ xs: 1, md: index % 2 === 0 ? 1 : 2 }}>
-        <Grid item md={6} xs={12}>
-          image here
-        </Grid>
-      </Box>
-    </Grid>
+    <>
+      <Typography variant="h2" align="center">
+        {title}
+      </Typography>
+      <Grid container>
+        <Box clone order={{ xs: 2, md: index % 2 === 0 ? 2 : 1 }}>
+          <Grid item md={6} xs={12}>
+            <Linkify
+              componentDecorator={(decoratedHref, decoratedText, key) => (
+                <a
+                  target="_blank"
+                  href={decoratedHref}
+                  key={key}
+                  rel="noopener noreferrer"
+                >
+                  {decoratedText}
+                </a>
+              )}
+            >
+              {description}
+            </Linkify>
+          </Grid>
+        </Box>
+        <Box clone order={{ xs: 1, md: index % 2 === 0 ? 1 : 2 }}>
+          <Grid item md={6} xs={12}>
+            image here
+          </Grid>
+        </Box>
+      </Grid>
+    </>
   );
 }
 
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Typography, Box, Container } from "@material-ui/core";
+import { Typography, Container } from "@material-ui/core";
 import Project from "./Project";
 
 function Projects() {
   return (
     <Container>
       <Typography variant="h2">Projects I have worked on</Typography>
-      <Project index={1} />
-      <Project index={2} />
-      <Project index={3} />
-      <Project index={4} />
+      <Project
+        index={1}
+        title="title"
+        description="description here https://www.google.com"
+      />
+      <Project
+        index={2}
+        title="title"
+        description="description here https://www.google.com"
+      />
+      <Project
+        index={3}
+        title="title"
+        description="description here https://www.google.com"
+      />
+      <Project
+        index={4}
+        title="title"
+        description="description here https://www.google.com"
+      />
     </Container>
   );
 }
