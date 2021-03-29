5e7b4482a909f9ac4d7cba4962de50fd726e67eb
Juggernaut9 <58269749+Juggernaut9@users.noreply.github.com>
2021-03-29 14:50:02 +0530

    npm run build not working (#22)
    * npm run build not working
    
    * fix bug

--- components/Projects/TechChips/TechChips.tsx
+++ components/Projects/TechChips/TechChips.tsx
@@ -2,18 +2,16 @@ import { memo } from "react";
 import { Grid } from "@material-ui/core";
 import Chip from "./Chip";
 
+import { StackType } from "../Projects";
+
 type appProps = {
-  stack?: {
-    label: string;
-    logo: string;
-    invert?: boolean;
-  }[];
+  stack: StackType[];
 };
 
 function TechChips({ stack }: appProps) {
   return (
     <Grid container spacing={2} style={{ maxWidth: "100%" }}>
-      {stack!.map((obj) => (
+      {stack.map((obj) => (
         <Grid key={obj.label} item xs={2} md={1}>
           <Chip label={obj.label} logo={obj.logo} invert={obj.invert} />
         </Grid>
