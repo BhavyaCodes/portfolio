4478726cecc1a3c14ee6c06b7285fe9e9d52fcf3
Bhavya <xbhavyatomar@gmail.com>
2021-04-08 16:16:36 +0530

    add spring transition

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -135,7 +135,11 @@ function projectsPage() {
       key="projects"
       initial={{ opacity: 0, x: -1000 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: -100 }}
+      exit={{ opacity: 1, x: -1000 }}
+      transition={{
+        x: { type: "spring", stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      }}
     >
       <Toolbar />
       <ProjectsTitle text="All projects I have worked on" />
