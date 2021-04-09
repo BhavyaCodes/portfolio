25eeb3ae3f0ee7b7ada4846604ab917ea343d427
Bhavya <xbhavyatomar@gmail.com>
2021-04-09 11:48:47 +0530

    add snackbar for sending message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,11 @@
-import { FormEvent, memo, useRef } from "react";
+import {
+  FormEvent,
+  memo,
+  useRef,
+  useState,
+  SyntheticEvent,
+  MouseEvent,
+} from "react";
 import {
   Container,
   Grid,
@@ -8,12 +15,15 @@ import {
   Button,
   Hidden,
   useTheme,
+  Snackbar,
+  IconButton,
 } from "@material-ui/core";
 import MailIcon from "@material-ui/icons/Mail";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import SendIcon from "@material-ui/icons/Send";
+import CloseIcon from "@material-ui/icons/Close";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -73,17 +83,27 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 function Footer() {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("Message Sent");
   const classes = useStyles();
   const theme = useTheme();
   const nameRef = useRef<HTMLInputElement>(null!);
   const emailRef = useRef<HTMLInputElement>(null!);
   const messageRef = useRef<HTMLInputElement>(null!);
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event: SyntheticEvent | MouseEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // console.log(nameRef.current.value);
-    // console.log(emailRef.current.value);
-    // console.log(messageRef.current.value);
     const formData = new FormData();
     formData.append("name", nameRef.current.value);
     formData.append("email", emailRef.current.value);
@@ -100,9 +120,12 @@ function Footer() {
       })
       .then((data) => {
         console.log(data);
+        setOpen(true);
       })
       .catch((error) => {
         console.log(error);
+        setMessage("Error sending message");
+        setOpen(true);
       });
   };
 
@@ -229,6 +252,28 @@ function Footer() {
                 send
               </Button>
             </form>
+            <Snackbar
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              open={open}
+              autoHideDuration={6000}
+              onClose={handleClose}
+              message={message}
+              action={
+                <>
+                  <IconButton
+                    size="small"
+                    aria-label="close"
+                    color="inherit"
+                    onClick={handleClose}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </IconButton>
+                </>
+              }
+            />
           </Grid>
         </Grid>
       </Container>
