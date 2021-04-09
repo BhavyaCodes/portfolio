5821c1ac96ea11b90d8d717a941556a02052cf3c
Bhavya <xbhavyatomar@gmail.com>
2021-04-09 22:06:34 +0530

    add icons and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -74,6 +74,26 @@ export default class MyDocument extends Document {
             property="twitter:image"
             content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/assets/programmer-meta.png`}
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
         </Head>
         <body>
           <Main />
