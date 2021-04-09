b2e296a294d09a9bec3b11ab4f9c6fd3691e39a3
Bhavya <xbhavyatomar@gmail.com>
2021-04-09 16:50:37 +0530

    change meta theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content="#4deb46" />
+          <meta name="theme-color" content="#161625" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
