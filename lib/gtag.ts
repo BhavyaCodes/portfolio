99e6af7ffcdf1ef0549b3028e6f9069421bdf23a
Juggernaut9 <58269749+Juggernaut9@users.noreply.github.com>
2021-04-11 13:47:33 +0530

    convert gtag.js to ts (#43)
    * converted to ts
    
    * fix url type from URL to string

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,14 +1,21 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID!;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
+export const pageview = (url: string) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
+export const event = ({ action, category, label, value }: GTagEvent) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
