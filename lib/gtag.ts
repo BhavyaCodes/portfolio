b5bb772e86fb797f780c13bb91187d1df5275a05
Bhavya <xbhavyatomar@gmail.com>
2021-04-11 13:12:40 +0530

    fix url type from URL to string

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,7 +1,7 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID!;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url: URL) => {
+export const pageview = (url: string) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
