b9b5308361554f6de2ead293893649b8dd4cd09a
Bhavya <xbhavyatomar@gmail.com>
2021-04-11 12:28:27 +0530

    add portfolio description

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -139,7 +139,14 @@ const projects: ProjectType[] = [
   },
   {
     title: "This website",
-    description: ["asdfasdf"],
+    description: [
+      "My portfolio website build with **React** using **Next.js** framework.",
+      "The source code is writting in **TypeScript** and the project is easy to maintain with the **type safety** provided by Typescript. The source code is very **moduler** allowing easy **debugging**.",
+      "Styling is done using **Material-UI**, the website comes with two **themes, dark and light.**",
+      "Animations are done using two libraries, **react-spring** for the bouncy logo animations, and **framer-motion** for page transitions.",
+      "**next/image** which is a new Next.js 10 feature is being used to automatically resize images according to display and also **prevent html elements shifting** from slow loading images.",
+      "This description is typed as **markdown** in source code, the Project component takes in props for links, icons, title, images, etc; nothing is hard coded.",
+    ],
     links: {
       live: "https://bhavya.dev",
       github: "https://github.com/Juggernaut9/portfolio",
